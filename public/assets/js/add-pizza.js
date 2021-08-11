7d99b4c8bed6b121cf30241bdc7060cf7a6c3041
araceli <carmona.araceli.4690@gmail.com>
2021-08-11 13:39:32 -0700

    added pwa offline add pizza

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -67,6 +67,8 @@ const handlePizzaSubmit = event => {
   })
   .catch(err => {
     console.log(err);
+    //data will be stored in browsers indexeddb database if connection fails
+    saveRecord(formData);
   });
 };
 
